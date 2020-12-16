4a8b02f29d06764ba4e0ce31074f43986c257ee7
Peter Sefton <pt@ptsefton.com>
2020-12-16 12:44:29 +1100

    Update datacrate_to_huni.py

--- datacrate_to_huni.py
+++ datacrate_to_huni.py
@@ -1,12 +1,25 @@
 """
 Quick and dirty script to push Datacrate to an experimental huni XML output
 
-HuNI has no standard input format this will need a custom harvester 
+HuNI has no standard input format this will need a custom harvester when I wrote this, the folks at Strategic Data handled that and we successfully imported data from Farms To Freeways.
 
-This converts all of the items with a @type RepostitoryObject into an XML file with the format
+This converts all of the items with a @type RepostitoryObject into an XML file with the following format - the first record is for a Person. A HuNI harvester can look for this...
 
 <datacrate>
-    <record> </record>
+    <record>
+     <meta name="@context" ref="">http://43.240.98.92/api-context</meta>
+     <meta name="@id" ref="">http://43.240.98.92/api/items/24</meta>
+     <meta name="@type" ref="">o:Item</meta>
+     <meta name="@type" ref="">schema:Person</meta>
+     <meta name="@type" ref="">RepositoryObject</meta>
+     <meta name="schema:url" ref="https://siouxziconnor.com">https://siouxziconnor.com</meta>
+     <meta name="dcterms:isPartOf" ref="http://43.240.98.92/api/item_sets/10">2003 Golden Eye Awards</meta>
+     <meta name="name" ref="">Siouxzi Connor</meta>
+     <meta name="description" ref="">Siouxzi Connor is an Australian-born writer and artist.
+Her first book was published by Repeater Books and distributed by Penguin Random House in early 2017, titled ‘Little Houses, Big Forests (desire is no light thing).’ It was launched in London at the Tenderbooks Gallery and then in New York at Spoonbill Books, and soon to launch with an exhibition in Australia. It received coverage on the BBC World Service.
+</meta>
+     <meta name="description" ref="https://www.imdb.com/name/nm2424588/">https://www.imdb.com/name/nm2424588/</meta>
+ </record>
 
     ...
     <record> </record>
